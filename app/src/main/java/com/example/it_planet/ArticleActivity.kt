634dc3431a6a004b74b4eb95d2d690c3f98c1a79
Sergey <kurilov.sereja2012@yandex.ru>
2021-04-04 13:34:30 +0300

    added the plugin to the class

--- app/src/main/java/com/example/it_planet/ArticleActivity.kt
+++ app/src/main/java/com/example/it_planet/ArticleActivity.kt
@@ -23,59 +23,20 @@ class ArticleActivity : Activity() {
         val link: String = getIntent().getStringExtra("link").toString()
         webView = findViewById(R.id.webView)
         webView.webViewClient = WebViewClient()
-        parseVKIT(link)
-        //webView.loadUrl(link)
+        parsers(link)
     }
 
-    private fun parseVKIT(url :String)
-    {
+    private fun parsers(url :String){
         Thread(Runnable {
-            val doc: Document = Jsoup.connect(url).get()
-            val articles: Elements = doc.select("article")
             val stringBuilder = StringBuilder()
-            val html: String = articles[0].html()
-            stringBuilder.append("<style type=\"text/css\">\n" +
-                    "   a { \n" +
-                    "   pointer-events: none;\n" +
-                    //"   color: #999;\n" +
-                    "   text-decoration: none;\n" +
-                    "   }\n" +
-                    "   img { \n" +
-                    "   height: auto;\n" +
-                    "   border: 0;\n" +
-                    "   max-width: 100%;\n" +
-                    "   }\n" +
-                    "blocks-gallery-item__caption {\n" +
-                    "position: absolute;\n" +
-                    "bottom: 0;\n" +
-                    "width: 100%;\n" +
-                    "max-height: 100%;\n" +
-                    "overflow: auto;\n" +
-                    "padding: 3em .77em .7em;\n" +
-                    "text-align: center;\n" +
-                    "font-size: .8em;\n" +
-                    "background: linear-gradient(0deg,rgba(0,0,0,.7),rgba(0,0,0,.3) 70%,transparent);\n" +
-                    "box-sizing: border-box;\n" +
-                    "margin: 0;\n" +
-                        "}\n" +
-                    ".blocks-gallery-grid, .wp-block-gallery {\n" +
-                    "display: flex;\n" +
-                   " flex-wrap: wrap;\n" +
-                    "list-style-type: none;\n" +
-                    "padding: 0;\n" +
-                   " margin: 0;\n" +
-                    "}\n" +
-                    "iframe {\n" +
-                    "max-width: 100%;\n" +
-                    "}\n" +
-                    "  </style>")
-            stringBuilder.append("<head>").append(articles[0].select("head").text()).append("</head>").append("<h1>").append(articles[0].select("h1").text()).append("</h1>").append(articles[0].select("div"))
+                if (url.substringAfter('.').substringBefore('.') == "guap") {
+                    val parserVKIT = ParserVKIT()
+                    stringBuilder.append(parserVKIT.parse(url))
+                }
             runOnUiThread {
                 webView.getSettings().setJavaScriptEnabled(true)
                 webView.loadData(stringBuilder.toString(), "text/html", null)
             }
         }).start()
     }
-
-
 }
